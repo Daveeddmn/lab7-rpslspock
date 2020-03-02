be62abe283d7f7bdde696374d754da40d7e43364
Daveeddmn <61674748+Daveeddmn@users.noreply.github.com>
2020-03-01 23:15:44 -0500

    Add files via upload

---
+++ Laab7/src/edu/cscc/RPSLSpockTestTest.java
@@ -0,0 +1,34 @@
+package edu.cscc;
+
+import static org.junit.Assert.*;
+
+public class RPSLSpockTestTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isValidPick() {
+    }
+
+    @org.junit.Test
+    public void generatePick() {
+    }
+
+    @org.junit.Test
+    public void isComputerWin() {
+    }
+
+    @org.junit.Test
+    public void isValidPick1() {
+    }
+
+    @org.junit.Test
+    public void generatePick1() {
+    }
+
+    @org.junit.Test
+    public void isComputerWin1() {
+    }
+}
---
+++ Laab7/src/edu/cscc/RSPLSpock.java
@@ -0,0 +1,60 @@
+package edu.cscc;
+import java.util.Random;
+
+//@David nguyen, this class lets the user pick a choice of 5 possible string answers. Rock, paper, scissors, lizard, spock.
+class RPSLSpock{
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    // This method makes sure that user choice is a valid answer.
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    // This method generates a random pick out of 1 Million, and chooses which case/ pick, that is.
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    // This method compares the computer choice to the human choice, and determines if it was a win.
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
